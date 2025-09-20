677c4897bb60e106cf9e5ed65a706e0ca5e690bb
Daniel Padrino <danswarrior1@gmail.com>
2025-09-20 10:26:56 -0300

    universal-router-sdk-deploy

--- sdks/universal-router-sdk/src/utils/getCurrencyAddress.ts
+++ sdks/universal-router-sdk/src/utils/getCurrencyAddress.ts
@@ -1,4 +1,4 @@
-import { Currency } from '@uniswap/sdk-core'
+import { Currency } from '@juiceswapxyz/sdk-core'
 import { ETH_ADDRESS } from './constants'
 
 export function getCurrencyAddress(currency: Currency): string {
--- sdks/universal-router-sdk/src/utils/pathCurrency.ts
+++ sdks/universal-router-sdk/src/utils/pathCurrency.ts
@@ -1,6 +1,6 @@
-import { Currency, Token } from '@uniswap/sdk-core'
-import { Pool as V4Pool } from '@uniswap/v4-sdk'
-import { TPool } from '@uniswap/router-sdk'
+import { Currency, Token } from '@juiceswapxyz/sdk-core'
+import { Pool as V4Pool } from '@juiceswapxyz/v4-sdk'
+import { TPool } from '@juiceswapxyz/router-sdk'
 
 export function getPathCurrency(currency: Currency, pool: TPool): Currency {
   // return currency if the currency matches a currency of the pool
