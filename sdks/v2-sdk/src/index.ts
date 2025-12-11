b5cc9074987f34fa994dcf81d1e57027abb20d27
Patrick <42653152+lapatric@users.noreply.github.com>
2025-12-11 15:50:14 +0100

    add V2 support and update deployment addresses (#6)

--- sdks/v2-sdk/src/index.ts
+++ sdks/v2-sdk/src/index.ts
@@ -1,4 +1,11 @@
-export { FACTORY_ADDRESS_MAP, INIT_CODE_HASH, MINIMUM_LIQUIDITY } from './constants'
+export {
+  FACTORY_ADDRESS_MAP,
+  INIT_CODE_HASH,
+  INIT_CODE_HASH_MAP,
+  LP_TOKEN_NAME_MAP,
+  LP_TOKEN_SYMBOL_MAP,
+  MINIMUM_LIQUIDITY,
+} from './constants'
 
 export * from './errors'
 export * from './entities'
